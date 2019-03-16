b939521e75f22cfd6d02cdeb96b01ae9a499489d
Iain Bean <iainmbean@gmail.com>
2019-03-16 17:17:53 +0000

    Intial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Screenshot URLs script
+
+A node script to take screenshots of webpages using puppeteer.
+
+## Installation
+
+Run `npm install` or `yarn install`
+
+## Generating screenshots
+
+The only required argument is URL. The output filename can be automatically generated.
+
+```
+node index.js [--url <url>] [-dest <path>] [--width <pixels>] [--height <pixels] [--format <format>]
+```
+
+e.g.
+
+```
+node index.js --url="https://www.google.com/" --width=1600 --height=900
+```
+
+will generate a screenshot with the dimensions 1600 by 900 pixels and output it here: /.output/20190316T171616_httpswww.google.com.jpg
---
+++ index.js
@@ -0,0 +1,55 @@
+const formatDate = require('date-fns/format');
+const puppeteer = require('puppeteer');
+const sanitize = require('sanitize-filename');
+let program = require('commander');
+
+// Parse arguments
+program
+  .version('0.1.0')
+  .option('-u, --url <path>', 'The URL of the page you want to screenshot')
+  .option('-d, --dest <path>', 'The output file path')
+  .option('-w, --width <pixels>', 'The viewport width in pixels', parseInt)
+  .option('-h, --height <pixels>', 'The viewport height in pixels', parseInt)
+  .option('-f, --format <format>', 'The output file format')
+  .parse(process.argv);
+
+/**
+ * Build a timestamped, sanitized filename
+ *
+ * @param {string} url
+ * @param {string} format
+ */
+function buildFilename(url, format) {
+  const currentDateTime = new Date();
+  const currentDateTimeToString = formatDate(
+    currentDateTime,
+    'YYYYMMDDTHHmmss'
+  );
+
+  return `./output/${currentDateTimeToString}_${sanitize(url)}.${
+    format === 'jpeg' ? 'jpg' : format
+  }`;
+}
+
+/**
+ *
+ * @param {string} url - The URL of the page you want to screenshot.
+ * @param {string} dest - The output file path.
+ * @param {number} width - The viewport width in pixels.
+ * @param {number} height - The viewport height in pixels.
+ * @param {number} format - The output file format.
+ */
+async function run(url, dest, width = 1920, height = 1080, format = 'jpeg') {
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+  const outputPath = dest || buildFilename(url, format);
+  await page.setViewport({ width: width, height: height });
+  await page.goto(url);
+  await page.screenshot({ path: outputPath, type: format });
+  await page.close();
+  await browser.close();
+}
+
+if (program.url) {
+  run(program.url, program.dest, program.width, program.height, program.format);
+}
